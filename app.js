f3735f88711b46f2c0906b7653f9909dd7a831da
Graf <jojo-2000@outlook.de>
2020-04-17 15:20:13 +0200

    Added Reset Button

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var button = document.getElementById('button');
+var reset = document.getElementById('test');
 var count = document.getElementById('count');
 
 button.addEventListener('click', () => {
   count.innerHTML++;
+})
+
+reset.addEventListener('click', () => {
+  count.innerHTML = 0;
 })
